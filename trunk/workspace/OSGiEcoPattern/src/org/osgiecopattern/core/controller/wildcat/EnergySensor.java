9edbd9b9bedfebdba2401d4319c65196f0b77714
guillaume.delamare@gmail.com <guillaume.delamare@gmail.com@7e414def-3cd0-4c6f-f098-a66c6d719fa7>
2012-06-06 12:32:53 +0000

    Ajout de Wildcat

---
+++ trunk/workspace/OSGiEcoPattern/src/org/osgiecopattern/core/controller/wildcat/EnergySensor.java
@@ -0,0 +1,17 @@
+package org.osgiecopattern.core.controller.wildcat;
+
+import org.ow2.wildcat.hierarchy.attribute.POJOAttribute;
+
+public class EnergySensor extends POJOAttribute {
+	
+	public EnergySensor(Object value) {
+		super(value);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public Object getValue() {
+		// TODO Auto-generated method stub
+		return super.getValue();
+	}
+}
